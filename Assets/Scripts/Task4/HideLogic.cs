afe73332afae3d4670b5d3915812980255b48b67
rudakov <rudakov@neolant.com>
2019-10-02 15:32:17 +0300

    Commit after work. Dunno

---
+++ Assets/Scripts/Task4/HideLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnBecameVisible()
+    {
+        enabled = true;
+    }
+    private void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+}
